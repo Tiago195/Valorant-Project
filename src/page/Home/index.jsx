68fbe00cadbb5d440a1a6a4c1d23425a89d4f353
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-01-31 21:52:44 -0300

    add fetch

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/index';
-// import fetch from '../../services/data/fetch'
+import fetch from '../../services/data/fetch'
 
 export default class index extends Component {
 
-  // componentDidMount() {
-  //   console.log(fetch().then((data) => console.log(data)))
-  // }
+  componentDidMount() {
+    console.log(fetch().then((data) => console.log(data)))
+  }
 
   render() {
     return (
