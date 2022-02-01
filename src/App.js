e8b7121507235ab05fe86253a1dfbd9f92e1f1f7
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-02-01 17:17:41 -0300

    add modal

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import Home from './page/Home/index';
 import Guns from './page/Guns/index'
+import Modal from '../src/components/Modal'
 
-class App extends Component  {
+class App extends Component {
   render() {
-  return (
-    <>
-      <Route exact path="/" render={ () => <Home /> } />
-      <Route path="/armas" component={ Guns } />
-    </>
-  );
+    return (
+      <>
+        <Route exact path="/" render={ () => <Home /> } />
+        <Route path="/armas" component={ Guns } />
+        <Route path="/modal" component={ Modal } />
+      </>
+    );
   }
 }
 
---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './index.css';
+const AGENT__MOCK = require('../../services/data/data')
+const { displayNome: { Breach } } = require('../../services/data/videos')
+
+
+export default class Modal extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      indexState: 0
+    }
+  }
+
+  handleClick = (index) => {
+    this.setState({ indexState: index })
+  }
+
+  render() {
+    const { indexState } = this.state
+    const { role, displayName, abilities } = AGENT__MOCK
+    return (
+      <div className='modal__container'>
+        <div className='conatainer__info-abilits'>
+          <section className='info-abilits__name-type'>
+            <div className='info-abilits__name-type--img'>
+              <img src={ role.displayIcon } alt={ role.displayName } />
+            </div>
+            <div className='info-abilits__name-type--data'>
+              <h2>{ displayName }</h2>
+              <h3>{ role.displayName }</h3>
+            </div>
+          </section>
+          <section className='info-abilits__img-abilits'>
+            { abilities.map(({ displayIcon, displayName }, index) => (
+              <div
+                key={ displayName }
+                onClick={ () => this.handleClick(index) }
+                className={ `info-abilits__img-abilits--img${index + 1}`
+                }>
+                <img src={ displayIcon } alt={ displayName } />
+              </div>
+            )) }
+          </section>
+          <section className='info-abilits__describe-abilits'>
+            <div className='info-abilits__describe-abilits--paragraph'>
+              <h3>{ abilities[indexState].displayName }</h3>
+              <p>{ abilities[indexState].description }</p>
+            </div>
+          </section>
+        </div >
+        <section
+          className='container__video'>
+          <video
+            key={ Breach[indexState] }
+            controls
+            autoPlay
+            muted
+            loop
+            poster="true"
+            width="500px"
+          >
+            <source src={ Breach[indexState] } type="video/mp4" />
+          </video>
+        </section>
+      </div >
+    )
+  }
+}
