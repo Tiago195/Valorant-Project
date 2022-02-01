6e40c71894048fbd76f86212ad4d9cf0ba3f2267
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-01-31 22:28:38 -0300

    resquests finish

--- src/services/data/fetch.js
+++ src/services/data/fetch.js
@@ -1,4 +1,24 @@
-export default async function requestCharacters() {
+export async function requestCharacters() {
   const response = await (await fetch('https://valorant-api.com/v1/agents?language=pt-BR&&isPlayableCharacter=true')).json()
+  return response
+}
+
+export async function requestWeapons() {
+  const response = await (await fetch('https://valorant-api.com/v1/weapons?language=pt-BR')).json()
+  return response
+}
+
+export async function requestWeaponsSkins() {
+  const response = await (await fetch('https://valorant-api.com/v1/weapons/skins?language=pt-BR')).json()
+  return response
+}
+
+export async function requestWeaponsSkinsChromas() {
+  const response = await (await fetch('https://valorant-api.com/v1/weapons/skinchromas?language=pt-BR')).json()
+  return response
+}
+
+export async function requestWeaponsSkinsLevel() {
+  const response = await (await fetch('https://valorant-api.com/v1/weapons/skinlevels?language=pt-BR')).json()
   return response
 }
