df939897b6e49d5e211524fddb3a7255e194dc43
Tiago Meireles <tiagocf475@gmail.com>
2022-01-31 21:57:39 -0300

    correcao

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/index';
 import fetch from '../../services/data/fetch'
+import './index.css';
 
 export default class index extends Component {
-
-  componentDidMount() {
-    console.log(fetch().then((data) => console.log(data)))
-  }
-
   render() {
     return (
       <>
         <Header pageAtual='inicio' />
+        <main className='main'>
+          <ul className='list-personagens'>
+            <li className='personagem'>teste</li>
+          </ul>
+          <section className='personagem-info'>
+            teste
+          </section>
+        </main>
       </>
     )
   }
